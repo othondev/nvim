f8bb38d5762b4d08af557d6ef57fcdeee0619e19
Othon Ferreira <othondev@gmail.com>
2023-11-11 18:06:53 +1000

    Fix packer setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,26 +1,17 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
--- Automatically install packer
+-- Ensure packer is installed
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
 	execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 end
 
-vim.cmd("packadd packer.nvim")
-
-return require("packer").startup(function(use)
-	-- Your plugins here
-	use("wbthomason/packer.nvim") -- Packer manages itself
-	use("nvim-lua/plenary.nvim") -- Common utilities
-
-	-- Add more plugins here
-
-	-- You can also specify plugin configuration directly
-	-- use {
-	--   'my/plugin',
-	--   config = function()
-	--     require('config.myplugin')
-	--   end
-	-- }
+--Install plugins
+require("packer").startup(function(use)
+	-- Core
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
 end)
+
+--Initialize plugins
