0a93696af4eb6e19d8a17c752a7dd1dedf7269a6
Othon Ferreira <othondev@gmail.com>
2023-11-12 13:12:16 +1000

    Added and lsp and theme too

---
+++ lua/plugins.lua
@@ -0,0 +1,48 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require("packer").startup(function(use)
+	-- Core
+	use("wbthomason/packer.nvim")
+	use("nvim-lua/plenary.nvim")
+
+	--theme
+	use({ "rose-pine/neovim", as = "rose-pine" })
+
+	--lsp
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
+	if packer_bootstrap then
+		require("packer").sync()
+	end
+end)
