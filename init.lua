f0668434fa943144c9eb8574af0392d24dd371bd
FIRST_NAME LAST_NAME <you@example.com>
2024-05-07 22:45:48 +1000

    feat: added new plugins.

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+require 'options'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -18,4 +18,23 @@ return {
     end,
     opts = {},
   },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+    cmd = 'Neotree',
+    opts = {
+      filesystem = {
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+          },
+        },
+      },
+    },
+  },
 }
