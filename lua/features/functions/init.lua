b935a051d05c9d2c11af196e0b49c93ef18b5d4f
FIRST_NAME LAST_NAME <you@example.com>
2024-05-08 23:15:38 +1000

    feat: new clean mapping

--- lua/features/functions/init.lua
+++ lua/features/functions/init.lua
@@ -1,5 +1,14 @@
 local builtin = require 'telescope.builtin'
 
-function SearchNvimFiles()
-  builtin.find_files { cwd = vim.fn.stdpath 'config' }
-end
+Search = {
+  NvimFiles = function()
+    builtin.find_files { cwd = vim.fn.stdpath 'config' }
+  end,
+
+  OpenFiles = function()
+    builtin.live_grep {
+      grep_open_files = true,
+      prompt_title = 'Live Grep in Open Files',
+    }
+  end,
+}
