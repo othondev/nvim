dba78b5894649d1f65208e59736f5eaa1203ad21
Othon Ferreira <othondev@gmail.com>
2024-05-12 20:55:02 +1000

    refactor(scope): the friendly autocomplete plugins

--- lua/features/plugins/code-completation.lua
+++ lua/features/plugins/code-completation.lua
@@ -3,16 +3,7 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {},
-      },
+      'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
@@ -56,6 +47,16 @@ return {
           { name = 'path' },
         },
       }
+    end,
+  },
+  {
+    'L3MON4D3/LuaSnip',
+    build = 'make install_jsregexp',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+    },
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
     end,
   },
 }
