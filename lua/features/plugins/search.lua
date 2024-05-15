64f7578432d55f2f6b18aa735d1c3d2c19b76935
Othon Ferreira <othon.ferreira@nextgen.net>
2024-05-15 11:02:09 +1000

    feat(search): avoid preview when the file is too large

--- lua/features/plugins/search.lua
+++ lua/features/plugins/search.lua
@@ -2,10 +2,20 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     config = function()
       require('telescope').setup {
         defaults = {
+          preview = {
+            filesize_hook = function(filepath, bufnr, opts)
+              local max_bytes = 5000
+              local cmd = { 'head', '-c', max_bytes, filepath }
+              require('telescope.previewers.utils').job_maker(cmd, bufnr, opts)
+            end,
+          },
           path_display = { 'smart' },
           mappings = {
             i = {
@@ -14,7 +24,16 @@ return {
             },
           },
         },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = 'smart_case',
+          },
+        },
       }
+      require('telescope').load_extension 'fzf'
     end,
   },
   { 'junegunn/fzf.vim', dependencies = {
