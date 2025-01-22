291cbbe5467b534ff7742159f081d7911b07563c
Othon Ferreira <othondev@gmail.com>
2025-01-22 22:05:02 +1000

    init setup with auto save and load

---
+++ ftplugin/lua.lua
@@ -0,0 +1,20 @@
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextYankPost" }, {
+    pattern = "*.lua",
+    callback = function()
+        if vim.bo.modified then
+            vim.defer_fn(function()
+                vim.cmd("silent! write")
+            end, 100)
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.lua",
+    callback = function()
+        local file = vim.fn.expand("%:p")
+        if file:match("init.lua$") then
+            vim.cmd("silent! so " .. file)
+        end
+    end,
+})
